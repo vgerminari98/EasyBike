ad134a2beefcaaf2e05cd3d3f8da24240e424aee
vgerminari98 <vgerminari98@outlook.com>
2021-09-30 22:33:28 -0300

    Implementação de estilo dos inputs de login

--- lib/pages/login.page.dart
+++ lib/pages/login.page.dart
@@ -12,7 +12,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple,
+      backgroundColor: Colors.white10,
       body: Padding(
           padding: EdgeInsets.all(10),
           child: Center(
@@ -20,13 +20,28 @@ class _LoginPageState extends State<LoginPage> {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
+                const Text(
+                  'EasyBike',
+                  style: TextStyle(
+                      color: Colors.deepPurple,
+                      fontSize: 30,
+                      fontFamily: 'VALORANT'),
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Divider(),
                 TextFormField(
                   autofocus: true,
                   keyboardType: TextInputType.text,
                   style: new TextStyle(color: Colors.white, fontSize: 30),
                   decoration: InputDecoration(
                     labelText: "Username",
-                    labelStyle: TextStyle(color: Colors.white)
+                    labelStyle: TextStyle(color: Colors.white),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(width: 3, color: Colors.deepPurple),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
                   ),
                 ),
                 Divider(),
@@ -36,8 +51,13 @@ class _LoginPageState extends State<LoginPage> {
                   keyboardType: TextInputType.text,
                   style: new TextStyle(color: Colors.white, fontSize: 30),
                   decoration: InputDecoration(
-                      labelText: "Password",
-                      labelStyle: TextStyle(color: Colors.white)
+                    labelText: "Password",
+                    labelStyle: TextStyle(color: Colors.white),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(width: 3, color: Colors.deepPurple),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
                   ),
                 ),
                 Divider(),
@@ -46,15 +66,25 @@ class _LoginPageState extends State<LoginPage> {
                     style: ElevatedButton.styleFrom(
                       primary: Colors.white,
                     ),
-                    child: const Text('Login', style: TextStyle(color: Colors.deepPurple),
-                  ),
-                  onPressed: () => { debugPrint('Pressionando')},
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(color: Colors.deepPurple),
+                    ),
+                    onPressed: () => {debugPrint('Pressionando')},
                   ),
                 ),
+                Divider(),
+                const Text('Or',
+                    style: TextStyle(color: Colors.deepPurple, fontSize: 18, fontFamily:'VALORANT'),
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.ellipsis),
+                Text('login with',
+                    style: TextStyle(color: Colors.deepPurple, fontSize: 16),
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.ellipsis)
               ],
             ),
-          )
-      ),
+          )),
     );
   }
 }
