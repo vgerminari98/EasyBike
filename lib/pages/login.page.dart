4da02b23a5b571cf3b0dd4ec1cf2b578b0f88811
vgerminari98 <vgerminari98@outlook.com>
2021-10-13 19:43:18 -0300

    refatoração tela de login

--- lib/pages/login.page.dart
+++ lib/pages/login.page.dart
@@ -13,78 +13,129 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white10,
-      body: Padding(
-          padding: EdgeInsets.all(10),
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'EasyBike',
-                  style: TextStyle(
-                      color: Colors.deepPurple,
-                      fontSize: 30,
-                      fontFamily: 'VALORANT'),
-                  textAlign: TextAlign.center,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Divider(),
-                TextFormField(
-                  autofocus: true,
-                  keyboardType: TextInputType.text,
-                  style: new TextStyle(color: Colors.white, fontSize: 30),
-                  decoration: InputDecoration(
-                    labelText: "Username",
-                    labelStyle: TextStyle(color: Colors.white),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(width: 3, color: Colors.deepPurple),
-                      borderRadius: BorderRadius.circular(15),
+      body: SingleChildScrollView(
+          child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/images/LOGO.png',
+                      alignment: Alignment.topLeft,
                     ),
-                  ),
-                ),
-                Divider(),
-                TextFormField(
-                  autofocus: true,
-                  obscureText: true,
-                  keyboardType: TextInputType.text,
-                  style: new TextStyle(color: Colors.white, fontSize: 30),
-                  decoration: InputDecoration(
-                    labelText: "Password",
-                    labelStyle: TextStyle(color: Colors.white),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(width: 3, color: Colors.deepPurple),
-                      borderRadius: BorderRadius.circular(15),
+                    Divider(),
+                    TextFormField(
+                      autofocus: true,
+                      keyboardType: TextInputType.text,
+                      style: new TextStyle(color: Colors.white, fontSize: 30),
+                      decoration: InputDecoration(
+                        labelText: "Username",
+                        labelStyle: TextStyle(color: Colors.white),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 3, color: Colors.deepPurple),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Divider(),
-                ButtonTheme(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.white,
+                    Divider(),
+                    TextFormField(
+                      autofocus: true,
+                      obscureText: true,
+                      keyboardType: TextInputType.text,
+                      style: new TextStyle(color: Colors.white, fontSize: 30),
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: TextStyle(color: Colors.white),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 3, color: Colors.deepPurple),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
                     ),
-                    child: const Text(
-                      'Login',
-                      style: TextStyle(color: Colors.deepPurple),
+                    Container(
+                      height: 40,
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        child: Text(
+                          "Recuperar Senha",
+                          textAlign: TextAlign.right,
+                          style: TextStyle(color: Colors.deepPurple),
+                        ),
+                        onPressed: () {},
+                      ),
                     ),
-                    onPressed: () => {debugPrint('Pressionando')},
-                  ),
+                    Divider(),
+                    ButtonTheme(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                        ),
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(color: Colors.deepPurple),
+                        ),
+                        onPressed: () => {debugPrint('Pressionando')},
+                      ),
+                    ),
+                    Divider(),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Text("Não possui conta?",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                  color: Colors.deepPurple, fontSize: 16),
+                              overflow: TextOverflow.ellipsis),
+                        ),
+                        Expanded(
+                          child: TextButton(
+                            child: Text("Inscrever-se",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                    color: Colors.deepPurple, fontSize: 16),
+                                overflow: TextOverflow.ellipsis),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      child: ButtonTheme(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.white,
+                          ),
+                          onPressed: () => {debugPrint('Pressionando')},
+                          child: Image.asset(
+                            'assets/images/fb-icon.png',
+                            height: 20,
+                            width: 20,
+                            alignment: Alignment.topLeft,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Column(children: <Widget>[
+                      Image.asset(
+                        'assets/images/fb-icon.png',
+                        height: 20,
+                        width: 20,
+                        alignment: Alignment.topLeft,
+                      ),
+                    ]),
+                  ],
                 ),
-                Divider(),
-                const Text('Or',
-                    style: TextStyle(color: Colors.deepPurple, fontSize: 18, fontFamily:'VALORANT'),
-                    textAlign: TextAlign.center,
-                    overflow: TextOverflow.ellipsis),
-                Text('login with',
-                    style: TextStyle(color: Colors.deepPurple, fontSize: 16),
-                    textAlign: TextAlign.center,
-                    overflow: TextOverflow.ellipsis)
-              ],
-            ),
-          )),
+              ))),
     );
   }
 }
